33e82c0acf79bbc3e3f90262326957c25f3a1902
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-04-11 02:29:03 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -200,7 +200,8 @@ def start(update: Update, context: CallbackContext):
                     send_settings(match.group(1), update.effective_user.id, True)
 
             elif args[0][1:].isdigit() and "rᴜʟᴇs" in IMPORTED:
-                IMPORTED["rᴜʟᴇs"].send_rules(update, args[0], from_pm=Tr ue)
+                IMPORTED["rᴜʟᴇs"].send_rules(update, args[0], from_pm=True)
+                
        else:
             first_name = update.effective_user.first_name
             
